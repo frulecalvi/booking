843d6cacb7c9b7a92535873f748abf190ec506fa
frulecalvi <frulecalvi@gmail.com>
2024-07-17 04:06:52 -0300

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => explode(',', env('CORS_PATHS', 'api/*,sanctum/csrf-cookie')),
+    'paths' => explode(',', env('CORS_PATHS', 'v1/*,sanctum/csrf-cookie')),
 
     'allowed_methods' => ['*'],
 
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
